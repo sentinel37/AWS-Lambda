6bb4eece473cd0fb5732e2cf93d3eaa2408019c8
Sean Isaac-Elder <sisaacelder@gmail.com>
2019-04-06 05:02:41 -0400

    Star instance(s) with AWS Lambda

---
+++ StartInstances.py
@@ -0,0 +1,18 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+def lambda_handler(event, context):
+
+     response=ec2.describe_instances()
+
+     for reservation in response["Reservations"]:
+
+        for instance in reservation["Instances"]:
+
+            id=[instance["InstanceId"]]
+
+            if instance["State"]["Name"]=="stopped":
+
+                ec2.start_instances(InstanceIds=id)
+      
